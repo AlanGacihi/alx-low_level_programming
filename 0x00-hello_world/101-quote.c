c5112ae815a404c8141df9a50da7a7e01b572a0e
AlanGacihi <alanmamicha@gmail.com>
2022-03-10 05:23:03 -0800

    First commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 /**
 *main - Entry point
@@ -7,6 +8,6 @@
 */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
