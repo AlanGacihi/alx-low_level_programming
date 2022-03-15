7ea06868ef0c168f1a09a59aa5734d2bab57658c
AlanGacihi <alanmamicha@gmail.com>
2022-03-15 10:36:57 -0700

    Task 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,20 +7,21 @@
 int main(void)
 {
 	long int i, t1, t2, next;
-        
+
 	t1 = 1;
 	t2 = 2;
 	next = t1 + t2;
 
 	printf("%li, %li, ", t1, t2);
 
-	for (i = 3; i <= 50; ++i)
+	for (i = 3; i < 50; ++i)
 	{
 		printf("%li, ", next);
 		t1 = t2;
 		t2 = next;
 		next = t1 + t2;
 	}
+	printf("%li", next);
 	printf("\n");
 	return (0);
 }
