3a7f592f287f7f36173ed40d5fa3ba8cf9044423
AlanGacihi <alanmamicha@gmail.com>
2022-03-16 02:30:38 -0700

    Task 16

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,22 +5,62 @@
 * @n: number input Ranges from 0-15 (inclusive)
 *
 */
+void print_number(int n);
 void print_times_table(int n)
 {
-	int row, col, step;
+	int a, i, row, col, count;
 
-	step = 0;
+	count = 0;
 
 	if (!(n > 15 || n < 0))
 	{
-		for (row = 0; row <= n; row++)
+		for (row = 0; row < n; row++)
 		{
-			for (col = 0; col <= n; col += step)
+			_putchar('0');
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(' ');
+		}
+		_putchar('0');
+		_putchar('\n');
+		for (row = 1; row <= n; row++)
+		{
+			_putchar('0');
+			_putchar(',');
+			for (col = row; col <= n * row; col += row)
 			{
-				_putchar((col + '0'));
+				a = col;
+				count = 0;
+				do {
+					a /= 10;
+					++count;
+				} while (a != 0);
+				for (i = 0; i < 4 - count; i++)
+				{
+					_putchar(' ');
+				}
+
+				print_number(col);
+				if (col != n * row)
+					_putchar(',');
 			}
-			step++;
 			_putchar('\n');
 		}
 	}
 }
+
+/**
+* print_times_table - a function that prints the input number digit by digit
+*
+* @n: number input
+*
+*/
+void print_number(int n)
+{
+	if (n / 10)
+	print_number(n / 10);
+
+	_putchar(n % 10 + '0');
+}
+
