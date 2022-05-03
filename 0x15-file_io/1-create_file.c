a5b0ff1e2c52d96190a612264a8764375f64a682
AlanGacihi <alanmamicha@gmail.com>
2022-05-03 11:56:05 -0700

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	wr = write(fd, text_content, strlen(text_content));
+	wr = write(fd, text_content, strlen(text_content) + 1);
 
 	if (wr == -1)
 		return (-1);
