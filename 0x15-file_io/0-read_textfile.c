a1772f7c2a11c2e209d2f032033122c131bfce79
AlanGacihi <alanmamicha@gmail.com>
2022-05-03 11:01:36 -0700

    Tread lightly, she is near

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed. It fails, returns 0.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t rd,  wr;
+	void *buf;
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+		return (0);
+
+	rd = read(fd, buf, letters);
+
+	wr = write(STDOUT_FILENO, buf, rd);
+
+	free(buf);
+	close(fd);
+
+	return (wr);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,5 @@
+Program 0 - Function that reads a text file and prints it to the POSIX standard output. <br>
+Program 1 - Function that creates a file. <br>
+Program 2 - Function that appends text at the end of a file. <br>
+Program 3 - Copies the content of a file to another file. <br>
+Program 100 - Displays the information contained in the ELF header at the start of an ELF file. <br>
