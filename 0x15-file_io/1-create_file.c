99e1be21257211dc1f36f4fcc09deb0c2f3abf1e
AlanGacihi <alanmamicha@gmail.com>
2022-05-03 23:21:21 -0700

    ...

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,13 +16,13 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	if (!text_content)
-		return  (-1);
-
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
+
+	if (!text_content)
+		text_content = "";
 
 	wr = write(fd, text_content, strlen(text_content));
 
