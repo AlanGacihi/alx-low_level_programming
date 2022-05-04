fdb5c8dbd1f5b83c791e49e76e0a726e7529c2d7
AlanGacihi <alanmamicha@gmail.com>
2022-05-03 23:17:26 -0700

    ...

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,10 +13,10 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	ssize_t wr;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
-	if (text_content == NULL)
+	if (!text_content)
 		return  (-1);
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
