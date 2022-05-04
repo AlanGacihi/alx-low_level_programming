93d803433fe2d3e8b11779684b786603fa709d95
AlanGacihi <alanmamicha@gmail.com>
2022-05-03 23:28:21 -0700

    ...

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,10 +23,12 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	wr = write(fd, text_content, strlen(text_content));
-
-	if (wr == -1)
-		return (-1);
+	if(text_content)
+	{
+		wr = write(fd, text_content, strlen(text_content));
+		if (wr == -1)
+			return (-1);
+	}
 
 	close(fd);
 
