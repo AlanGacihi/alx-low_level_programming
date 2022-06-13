5ba5ad51eaee35c16fc8ee3ad2273a5e7fc2e2d5
AlanGacihi <alanmamicha@gmail.com>
2022-06-13 12:54:39 -0700

    Fix 2

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/AlanGacihi/alx-low_level_programming/raw/master/0x18-dynamic_libraries/inject.so -P ../
-export LD_PRELOAD=$PWD/../inject.so
+wget https://github.com/AlanGacihi/alx-low_level_programming/blob/main/0x18-dynamic_libraries/hack.so -P ../
+export LD_PRELOAD=$PWD/../hack.so
