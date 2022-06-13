8a2810023636d985f8d7c2ee25b4864a34f57639
AlanGacihi <alanmamicha@gmail.com>
2022-06-13 12:58:10 -0700

    Remove

--- 0x18-dynamic_libraries/100-tests.py
+++
@@ -1,7 +0,0 @@
-import random
-import ctypes
-
-cops = ctypes.CDLL('./libthre.so')
-a = random.randint(-111, 111)
-b = random.randint(-111, 111)
-print(cops._puts("Hello"))
--- 0x18-dynamic_libraries/101-make_me_win.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget https://github.com/AlanGacihi/alx-low_level_programming/blob/main/0x18-dynamic_libraries/hack.so -P ../
-export LD_PRELOAD=$PWD/../hack.so
